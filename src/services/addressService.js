ea55636379971df8d531bb49ffa19904c0ba4ae4
interns-ecaps <interns.ecaps@gmail.com>
2025-07-03 11:22:35 +0530

    .

--- src/services/addressService.js
+++ src/services/addressService.js
@@ -1,10 +1,9 @@
-// src/services/addressService.js
+import api from '../utils/axiosInstance';
 
-import axiosInstance from '../utils/axiosInstance';
+export const getMyAddresses = () => api.get('/user/v1/addresses/');
 
-export const getMyAddresses = async () => {
-  const response = await axiosInstance.get('/user/v1/addresses');
-  return response.data.results;
-};
+export const getAddressesByUserId = (userId) =>
+  api.get('/user/v1/addresses/', { params: { user_id: userId } });
 
-export const getAddressesByUser = (userId) => axiosInstance.get(`/user/v1/addresses/${userId}`);
+export const getUserByEmail = (email) =>
+  api.get('/user/v1/users/', { params: { email } });
