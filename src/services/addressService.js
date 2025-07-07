34d13abd773c65c0ed4ed5894864e96760e5020a
interns-ecaps <interns.ecaps@gmail.com>
2025-07-07 12:47:16 +0530

    address issue solved

--- src/services/addressService.js
+++ src/services/addressService.js
@@ -1,26 +1,25 @@
 import api from '../utils/axiosInstance';
 
-export const getMyAddresses = () => api.get('/user/v1/addresses/');
-
 export const getAddressesByUserId = (userId) =>
   api.get('/user/v1/addresses/', { params: { user_id: userId } });
 
-export const getAddressesByUser = (userId) => axiosInstance.get(`/user/v1/addresses/${userId}`);
+export const getMyAddresses = () => api.get('/user/v1/addresses/');
+
+export const getAddressesByUser = (userId) => api.get(`/user/v1/addresses/${userId}`);
 
 export const createAddress = async (addressData) => {
-  const response = await axiosInstance.post("/user/v1/create_address", addressData);
+  const response = await api.post("/user/v1/create_address", addressData);
   return response.data;
 };
 
-
 // PUT (replace entire address)
 export const updateAddress = async (addressId, updatedData) => {
-  const response = await axiosInstance.put(`/user/v1/replace_address/${addressId}`, updatedData);
+  const response = await api.put(`/user/v1/replace_address/${addressId}`, updatedData);
   return response.data;
 };
 
 // PATCH (update specific fields)
 export const patchAddress = async (addressId, patchData) => {
-  const response = await axiosInstance.patch(`/user/v1/update_address/${addressId}`, patchData);
+  const response = await api.patch(`/user/v1/update_address/${addressId}`, patchData);
   return response.data;
 };
